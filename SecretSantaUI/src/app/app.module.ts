509cbab0aab26535f07cc43a23e9c34fa219d328
Riley <rileybunce1@gmail.com>
2020-12-11 23:25:32 -0800

    add new components to app module

--- SecretSantaUI/src/app/app.module.ts
+++ SecretSantaUI/src/app/app.module.ts
@@ -11,14 +11,16 @@ import { CreateUserComponent } from './create-user/create-user.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { GroupsComponent } from './group/groups/groups.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-
+import { AddGroupComponent } from './add-group/add-group.component';
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
     CreateUserComponent,
     GroupsComponent,
-    NavBarComponent
+    NavBarComponent,
+    AddGroupComponent,
+    
   ],
   imports: [
     BrowserModule,
